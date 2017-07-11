a9ea542bae292505aa848e9eaa32363a8b518e8b
Samantha Rie <samantha.rie@gmail.com>
2017-07-11 15:25:21 -0300

    primeiro commit

---
+++ ex.3/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Primeiro, Segundo} from './component.jsx'
+
+ReactDOM.render(
+    <div>
+        <Primeiro />
+        <Segundo />
+    </div>
+    , document.getElementById('app'))
